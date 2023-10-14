584190acd09f73c874f0b7b1b0b5eb79cd08a3b5
Luca <Dikhanbaev.2018@mail.ru>
2023-10-14 16:42:19 +0600

    Add biography.js

--- src/pages/Biography.js
+++ src/pages/Biography.js
@@ -4,21 +4,26 @@ import './style.css';
 const Biography = () => {
     function Player({FIO, tel, email, city, experience, skills, srcImg}){
         return (
-            <div className="Main">
-                <h1>FIO: {FIO}</h1>
-                <h3>tel: {tel}</h3>
-                <h3>email: {email}</h3>
-                <h3>city: {city}</h3>
-                <p>experience: {experience}</p>
-                <p>skills: {skills}</p>
-                img: <img src={require(srcImg)}></img>
+            <div className="container1">
+                <h1 className="title1">Biography</h1>
+                <div className="block1">
+                    <img src={require("./photo/me.jpg")}></img>
+                    <div>
+                        <h1>FIO: {FIO}</h1>
+                        <h3>tel: {tel}</h3>
+                        <h3>email: {email}</h3>
+                        <h3>city: {city}</h3>
+                        <h3>experience: {experience}</h3>
+                        <h3>skills: {skills}</h3>
+                    </div>
+                </div>
             </div>
         );
     }//Player()
 
     return (
         <Player
-            FIO={"Нурбек Алимхан Нұрымұлы"}
+            FIO={"Нурбек Алимхан Нурымулы"}
             tel={"87774564633"}
             email={"dikhanbaev.2019@mail.ru"}
             city={"Astana"}
